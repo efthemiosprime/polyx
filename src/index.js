017fb91b26450a08161a003d7614890342c05bd4
efthemios (bong) suyat <efthemios.suyat@gmail.com>
2025-02-27 11:14:47 -0500

    update how to export

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ export { Either } from './core/either.js';
 export { ArrayTransform } from './core/arrayTransform.js';
 // Re-export full modules for dynamic imports
 export * as data from './data/index.js';
+export * as core from './core/index.js';
 export * as dom from './dom/index.js';
 export * as async from './async/index.js';
 // export * as advanced from './advanced/index.js';
