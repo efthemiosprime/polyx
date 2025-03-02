1ae1e0a155e0e2d2b033edcc163e55091b178468
efthemios (bong) suyat <efthemios.suyat@gmail.com>
2025-03-02 09:00:27 -0500

    either added fromCondition to handles any boolean value

--- src/core/either.js
+++ src/core/either.js
@@ -19,13 +19,24 @@ export const Either = {
       getOrElse: _ => x
     }),
     
+    // specifically handles null/undefined checks
     fromNullable: x => x != null ? Either.Right(x) : Either.Left(null),
     
+    // is for capturing exceptions from functions that might throw
     tryCatch: (f) => {
       try {
         return Either.Right(f());
       } catch (e) {
         return Either.Left(e);
       }
+    },
+
+    // handles any boolean condition
+    fromCondition: (condition, leftValue, rightValue) => {
+      // If leftValue and rightValue are not provided, use null as default
+      const left = leftValue !== undefined ? leftValue : null;
+      const right = rightValue !== undefined ? rightValue : null;
+      
+      return condition ? Either.Right(right) : Either.Left(left);
     }
   };
