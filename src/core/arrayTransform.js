542ec34b2aad6efa356fe237552f83866dbfa35f
efthemios (bong) suyat <efthemios.suyat@gmail.com>
2025-03-01 20:35:46 -0500

    ArrayTransform: filter implementation

--- src/core/arrayTransform.js
+++ src/core/arrayTransform.js
@@ -3,6 +3,7 @@ export const ArrayTransform = {
     from: arr => ({
       value: Array.from(arr),
       map: fn => ArrayTransform.from(Array.from(arr).map(fn)),
+      filter: fn => ArrayTransform.from(Array.from(arr).filter(fn)),
       forEach: fn => { Array.from(arr).forEach(fn); return ArrayTransform.from(arr); },
       toSet: () => new Set(Array.from(arr)),
       toArray: () => Array.from(arr)
